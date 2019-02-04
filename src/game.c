40cc0723ddab0c2e079e26c822cf5b4f9e52aa86
William Setzer <wjsetzer@uab.edu>
2019-02-03 22:43:32 -0600

    changed to hardcoded threads

--- src/game.c
+++ src/game.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <time.h>
 #include <omp.h>
-#include <sys/sysinfo.h>
+// #include <sys/sysinfo.h>
 
 #include "game.h"
 
@@ -114,7 +114,8 @@ void simgen(board_t board) {
   // copy old board to new board
   copyboard(temp, board);
 
-  omp_set_num_threads(get_nprocs());
+  // omp_set_num_threads(get_nprocs());
+  omp_set_num_threads(16);
 
   # pragma omp parallel for
   for (size_t row = 1; row <= board.row; row++) {
