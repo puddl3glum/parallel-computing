fc36cd4f299e3ea6f6c43280e3ca5a0b9b263e5e
William Setzer <wjsetzer@uab.edu>
2019-02-26 21:02:30 -0600

    fixed data race on inc/dec neighbors

--- src/game.c
+++ src/game.c
@@ -3,26 +3,110 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <time.h>
 #include <omp.h>
 #include <sys/sysinfo.h>
 
 #include "game.h"
+
+#define ANSI_COLOR_INVERT "\x1b[7m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 void incneighbors(board_t, const uint64_t, const uint64_t);
 void decneighbors(board_t, const uint64_t, const uint64_t);
 void copyboard(board_t, board_t);
 board_t blankboard(const uint64_t, const uint64_t);
 cell_t** blankcells(const uint64_t, const uint64_t);
+bool arraycomp(uint64_t*, uint64_t*, uint64_t);
 
+cyclesum_t newcyclesum(const uint64_t row, const uint64_t col, uint64_t maxcycles) {
+  /*
+   * Return a new cyclesum_t struct
+   * */
+
+  maxcycles = maxcycles + 1; // add 1 to make space for the cycle being summed
+
+  // create an array for holding sums of length maxcycle
+  uint64_t** const sums = calloc(sizeof(uint64_t*), maxcycles);
+
+  for (size_t x = 0; x < maxcycles; x++) {
+
+    // create an array for holding row sums of length col
+    sums[x] = calloc(sizeof(uint64_t), col);
+
+  }
+
+  cyclesum_t cyclesum = {
+    maxcycles,
+    col,
+    row,
+    0, // current cycle is currently 0
+    sums
+  };
+
+  return cyclesum;
+}
+
+void freecyclesum(cyclesum_t cyclesum) {
+  for (size_t x = 0; x < cyclesum.maxcycle; x++) {
+    free(cyclesum.sums[x]);
+  }
+
+  free(cyclesum.sums);
+}
+
+bool arraycomp(uint64_t* a, uint64_t* b, uint64_t len) {
+
+  for (size_t x = 0; x < len; x++) {
+    if (a[x] != b[x]) return false;
+  }
+
+  return true;
+}
+
+bool checkcycles(cyclesum_t* cyclesum, board_t board) {
+  /* Returns true if two cycles are equal, else false
+   * Sets the current state of the board to be the next
+   * cycle, replacing the oldest cycle
+   * */
+
+  // sum the alive cells in the rows of the board
+
+  for (size_t x = 1; x <= board.col; x++ ) {
+    uint64_t sum = 0;
+
+    for (size_t y = 1; y <= board.row; y++) {
+      if (board.cells[x][y].alive) {
+        sum++;
+      }
+    }
+
+    // set sum to cyclesum current
+    cyclesum->sums[cyclesum->current][x - 1] = sum;
+  }
+
+  // compare current to other cycles
+  for (size_t x = 0; x < cyclesum->maxcycle; x++) {
+    uint64_t currentcycle = cyclesum->current;
+
+    if (x != currentcycle) {
+      if ( arraycomp(cyclesum->sums[x], cyclesum->sums[currentcycle], cyclesum->row)) return true;
+    }
+  }
+
+  // increment cycle
+  // NOTE: there is no need to increment if two cycles matched
+  cyclesum->current = (cyclesum->current + 1) % cyclesum->maxcycle;
+
+  return false;
+}
 
 cell_t** blankcells(const uint64_t row, const uint64_t col) {
 
-  cell_t** cells = calloc(sizeof(cell_t*), col+2);
+  cell_t** cells = calloc(sizeof(cell_t*), row+2);
 
-  for (size_t r = 0; r < col+2; r++) {
-    cell_t* rowptr = calloc(sizeof(cell_t), row+2);
-    cells[r] = rowptr;
+  for (size_t r = 0; r < row+2; r++) {
+    cell_t* colptr = calloc(sizeof(cell_t), col+2);
+    cells[r] = colptr;
   }
 
   return cells;
@@ -44,7 +128,7 @@ board_t blankboard(const uint64_t row, const uint64_t col) {
 
 }
 
-board_t randomboard(const uint64_t rowlen, const uint64_t colsize) {
+board_t randomboard(const uint64_t rowlen, const uint64_t colsize, const uint32_t seed) {
   /*
    * Return a 1-dimensional array (N+2)x(M+2) long with random values.
    * The values are initialized from 1..N and 1..M.
@@ -53,15 +137,18 @@ board_t randomboard(const uint64_t rowlen, const uint64_t colsize) {
 
   // First and last N+2 are false.
   // For every chunk, the first and last are false.
+  // printf("%d\n", seed);
   
-  srand((uint32_t)time(NULL));
+  srand(seed);
 
   board_t board = blankboard(rowlen, colsize);
 
   for (size_t row = 1; row <= board.row; row++) {
     for (size_t col = 1; col <= board.col; col++) {
-      bool val = rand() & 1;
-      if (val) {
+      int n = rand();
+      bool val = n % 2 == 0;
+      // printf("%d", val);
+      if (val == 1) {
         board.cells[row][col].alive = true;
         // not necessarily true, but ensure the cell will be
         // examined in the first iteration
@@ -85,7 +172,8 @@ void incneighbors(board_t board, const uint64_t row, const uint64_t col) {
       if (x == 0 && y == 0) continue;
 
       // printf("\tCell neighbor: (%ld, %ld)\n", col + y, row + x);
-      board.cells[(int64_t) row + y][(int64_t) col + x].neighbors++;
+      # pragma omp atomic
+      board.cells[(int64_t) row + x][(int64_t) col + y].neighbors++;
     }
   }
 }
@@ -101,12 +189,14 @@ void decneighbors(board_t board, const uint64_t row, const uint64_t col) {
 
       if (x == 0 && y == 0) continue;
 
-      board.cells[(int64_t) row + y][(int64_t) col + x].neighbors--;
+      # pragma omp atomic
+      board.cells[(int64_t) row + x][(int64_t) col + y].neighbors--;
     }
   }
 }
 
 void simgen(board_t board) {
+
 
   // create temp board
   board_t temp = blankboard(board.row, board.col);
@@ -143,31 +233,44 @@ void simgen(board_t board) {
 }
 
 void copyboard(board_t dest, board_t src) {
-  for (size_t row = 0; row < src.col + 2; row++) {
-    memcpy(dest.cells[row], src.cells[row], sizeof(cell_t) * src.row + 2);
+  /*
+  for (size_t row = 0; row < src.row + 2; row++) {
+     memcpy(dest.cells[row], src.cells[row], sizeof(cell_t) * src.col + 2);
   }
+  */
+  
+  
+  for (size_t row = 1; row <= src.row; row++) {
+    for (size_t col = 1; col <= src.col; col++) {
+      dest.cells[row][col] = src.cells[row][col];
+    }
+  }
+  
 }
 
 void printboard(board_t board) {
-  for (size_t i = 1; i <= board.col; i++ ){
-    for (size_t j = 1; j <= board.row; j++) {
-      printf(board.cells[i][j].alive ? "█" : "░");
+  for (size_t i = 1; i <= board.row; i++ ){
+    for (size_t j = 1; j <= board.col; j++) {
+      // printf(board.cells[i][j].alive ? "█" : "░");
     }
     puts("");
   }
 }
 
 void printfullboard(board_t board) {
-  for (size_t i = 0; i < board.col + 2; i++ ){
-    for (size_t j = 0; j < board.row + 2; j++) {
-      printf(board.cells[i][j].alive ? "█" : "░");
+  for (size_t i = 0; i < board.row + 2; i++ ){
+    for (size_t j = 0; j < board.col + 2; j++) {
+      // printf(board.cells[i][j].alive ? "█" : "░");
+      if (board.cells[i][j].alive) printf(ANSI_COLOR_INVERT);
+      printf("%d", board.cells[i][j].neighbors);
+      printf(ANSI_COLOR_RESET);
     }
     puts("");
   }
 }
 
 void freecells(board_t b) {
-  for (size_t i = 0; i < b.col + 2; i++) {
+  for (size_t i = 0; i < b.row + 2; i++) {
     free(b.cells[i]);
   }
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "main.h"
 #include "game.h"
@@ -12,20 +14,50 @@
 
 int main(int argc, char* argv[]) {
 
-  if (argc < 4) {
-    printf("USAGE: %s width height generation\n", argv[0]);
-    return 1;
+  int opt;
+  uint32_t seed = (uint32_t) time(NULL);
+
+
+  while ((opt = getopt(argc, argv, "s:")) != -1) {
+    switch(opt) {
+      case 's':
+        seed = (uint32_t) strtoul(optarg, NULL, 10);
+        break;
+      default:
+        fprintf(stderr, "USAGE: %s [OPTION...] width height generations\n", argv[0]);
+        break;
+    }
+  
   }
+
+
+  if (optind >= argc) {
+    fprintf(stderr, "Expected arguments after options");
+    exit(EXIT_FAILURE);
+  }
+
+
+  uint64_t width = strtoul(argv[0 + optind], NULL, 10);
+  uint64_t height = strtoul(argv[1 + optind], NULL, 10);
+  uint64_t generations = strtoul(argv[2 + optind], NULL, 10);
+
+  const uint64_t maxcycles = 12;
+
+  // printf("%ld %ld %ld\n", width, height, generations);
+
+  // printf("%ld\n", width);
+
+  // if (argc < 4) {
+  //  return 1;
+  // }
+
 
   // int boardwidth = 1000;
   // int boardheight = 1000;
 
   // int maxgenerations = 1000;
   
-  uint64_t boardwidth = strtoul(argv[1], NULL, 10);
-  uint64_t boardheight = strtoul(argv[2], NULL, 10);
-  uint64_t maxgenerations = strtoul(argv[3], NULL, 10);
-
+  // uint32_t seed = (uint32_t) strtoul(argv[4], NULL, 10);
 
 #ifdef DEBUG
   // SDL_Event event;
@@ -33,25 +65,31 @@ int main(int argc, char* argv[]) {
   SDL_Window* window;
 
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer((int) boardwidth, (int) boardheight, 0, &window, &renderer);
+  SDL_CreateWindowAndRenderer((int) width, (int) height, 0, &window, &renderer);
 #endif
+
 
   // Read in board state
   // OR
   // Randomize board state
-  board_t board = randomboard(boardwidth, boardheight);
+  board_t board = randomboard(width, height, seed);
 
-  for (uint64_t gen = 0; gen < maxgenerations; gen++) {
+  // get new cyclesum tracker
+  cyclesum_t cyclesum = newcyclesum(width, height, maxcycles);
+
+  for (uint64_t gen = 0; gen < generations; gen++) {
 
 #ifdef DEBUG
     // Visualize
     drawboard(renderer, board);
-    // printboard(board);
+    // printfullboard(board);
     // puts("");
 #endif
   
     // Simulate generation
     simgen(board);
+
+    if ( checkcycles(&cyclesum, board) ) break;
     
     // break;
   }
@@ -64,5 +102,6 @@ int main(int argc, char* argv[]) {
   
   // cleanup
   freecells(board);
+  freecyclesum(cyclesum);
 }
 
