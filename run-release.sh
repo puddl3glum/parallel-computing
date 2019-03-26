e678e7d5729517256db8a9ed924bc2fda6a6a33b
William Setzer <wjsetzer@uab.edu>
2019-03-26 13:10:39 -0500

    Changed run-release.sh to take dimensions into account

--- run-release.sh
+++ run-release.sh
@@ -4,12 +4,14 @@ total=0
 
 runs=3
 
-dim=5000
+threads=10
+width=5000
+height=5000
 gen=5000
 
 for i in `seq 1 $runs`;
 do
-        time_taken=$( { /usr/bin/time -f "%e" ./bin/conway -s 1 $dim $dim $gen; } 2>&1 )
+        time_taken=$( { /usr/bin/time -f "%e" ./bin/conway -s 1 -t $threads $width $height $gen; } 2>&1 )
         # time_taken=$( { /usr/bin/time -f "%e" ./bin/conway $1 $1 $2; } 2>&1 )
         echo $time_taken
         total=$(bc -l <<< "$total + $time_taken")
